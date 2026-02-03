7a39b18b691444973c4f277e85ac62aef7fb130f
Jacopo Parretti <117900254+djacoo@users.noreply.github.com>
2026-02-03 09:58:17 +0100

    Add KNN model with optimized hyperparameters (81.17% test accuracy)

---
+++ src/models/knn.py
@@ -0,0 +1,60 @@
+from typing import Dict, Optional, Union
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+from .base_model import BaseModel
+from .model_registry import register_model
+
+
+@register_model(
+    name='knn',
+    description='K-Nearest Neighbors classifier',
+    category='instance-based',
+    requires_probability=True
+)
+class KNNModel(BaseModel):
+
+    def __init__(
+        self,
+        n_neighbors: int = 15,
+        weights: str = 'distance',
+        algorithm: str = 'auto',
+        metric: str = 'minkowski',
+        p: int = 2,
+        n_jobs: int = -1,
+        **kwargs
+    ):
+        hyperparameters = {
+            'n_neighbors': n_neighbors,
+            'weights': weights,
+            'algorithm': algorithm,
+            'metric': metric,
+            'p': p,
+            'n_jobs': n_jobs,
+            **kwargs
+        }
+        super().__init__(model_name='KNN', **hyperparameters)
+
+    def _build_model(self) -> KNeighborsClassifier:
+        return KNeighborsClassifier(**self.hyperparameters)
+
+    def _fit(
+        self,
+        X_train: Union[np.ndarray, pd.DataFrame],
+        y_train: Union[np.ndarray, pd.Series],
+        X_val: Optional[Union[np.ndarray, pd.DataFrame]] = None,
+        y_val: Optional[Union[np.ndarray, pd.Series]] = None,
+        verbose: bool = True
+    ) -> Dict[str, float]:
+        self.model.fit(X_train, y_train)
+
+        y_train_pred = self.model.predict(X_train)
+        train_accuracy = accuracy_score(y_train, y_train_pred)
+
+        metrics = {
+            'accuracy': train_accuracy,
+        }
+
+        return metrics
